376855a48a4003818e784267f7bf5eb70ca8180f
SavantLogics <rsaviant@gmail.com>
2021-09-16 23:31:15 -0700

    Added enumerate method

---
+++ Automate_the_Boring_Stuff/Enumerate.py
@@ -0,0 +1,8 @@
+# Using the enumerate() Function with Lists
+
+supplies = ['pens', 'staplers', 'pencils', 'paper', 'binders']
+
+for index, item in enumerate(supplies):
+    print('Index ' + str(index) + ' in supplies is: ' + item)
+
+    
