6022fdf4338f279a61b423d4c3a03183bde3d83e
SavantLogics <rsaviant@gmail.com>
2021-09-28 22:19:42 -0700

    Iteratiing Enumerating

--- Automate_the_Boring_Stuff/Enumerate.py
+++ Automate_the_Boring_Stuff/Enumerate.py
@@ -5,4 +5,14 @@ supplies = ['pens', 'staplers', 'pencils', 'paper', 'binders']
 for index, item in enumerate(supplies):
     print('Index ' + str(index) + ' in supplies is: ' + item)
 
-    
+
+
+list = ['batman', 'superman', 'joker', 'lex luther']
+
+for hero in list:
+    print(hero)
+
+for villain in enumerate(list):
+    print(villain)
+
+# ${fileDirname}
