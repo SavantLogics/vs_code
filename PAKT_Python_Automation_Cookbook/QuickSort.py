1f2524fb44662f7773b2ae7e13e1e4c1359a9fd0
SavantLogics <rsaviant@gmail.com>
2021-10-27 20:41:06 -0700

    J-Notebook in VS Code. Copied quick sort algorithm

--- PAKT_Python_Automation_Cookbook/QuickSort.py
+++ PAKT_Python_Automation_Cookbook/QuickSort.py
@@ -1,17 +1,30 @@
+"""A pure Python implementation of quick sort algorithm
+    :param collection: a mutable collection of comparable items
+    :return: the same collection ordered by ascending
+    Examples:
+    >>> quick_sort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+    >>> quick_sort([])
+    []
+    >>> quick_sort([-2, 5, 0, -45])
+    [-45, -2, 0, 5]
+    """
 
-array = [10, 8, 6, 4, 2]
-def quicksort(array):
-    if len(array) < 2:
-        # Base case: arrays with 0 or 1 element are already "sorted"
-        return array 
-    else:
-        # Recursive case
-        pivot = array(2)
-        # Sub-array of all the elements less than the pivot  
-        less = [i for i in array[1:] if i <= pivot] 
-        # Sub-array of all the elements greater than the pivot 
-        greater = [i for i in array[1:] if i > pivot] 
-        return quicksort(less) + [pivot] + quicksort(greater)
+from __future__ import annotations
 
-    print(quicksort) 
-#quicksort(array)
+def quick_sort(collection: list) -> list:
+    
+    if len(collection) < 2:
+        return collection
+    pivot = collection.pop()  # Use the last element as the first pivot
+    greater: list[int] = []  # All elements greater than pivot
+    lesser: list[int] = []  # All elements less than or equal to pivot
+    for element in collection:
+        (greater if element > pivot else lesser).append(element)
+    return quick_sort(lesser) + [pivot] + quick_sort(greater)
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(quick_sort(unsorted))
